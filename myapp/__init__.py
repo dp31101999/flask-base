b8e506a7529a677625f73572a29a882e58ba9c93
Daniel <dp3138467@gmail.com>
2024-11-15 14:04:00 -0600

    Primer commit

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,14 +1,16 @@
 ''' Este archivo se utiliza para realizar la importacion de las configuraciones globales
     tales como la importacion de la app, las rutas generales del proyecto, definir si el
     servidor corre en modo debug, Registro de la BD, etc.'''
-from flask import Flask
+from flask import Flask, render_template, request
 from myapp.config import DevConfig
 from myapp.task.controllers import taskRoute
 
-app = Flask(__name__)
+app = Flask(__name__) #template_folder="/pages"
 app.register_blueprint(taskRoute)
 #app.debug = True
 app.config.from_object(DevConfig)
+
 @app.route('/')
 def hello_world() -> str:
-    return ' Hello world'
+    name = request.args.get('name', 'Valor por defecto')
+    return render_template('index.html', task="Josue", name=name)
--- myapp/task/controllers.py
+++ myapp/task/controllers.py
@@ -1,24 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
 
-taskRoute = Blueprint('task', __name__, url_prefix='/task')
-#Creacion de rutas en el modulo rutas
+taskRoute = Blueprint('task', __name__, url_prefix='/task')  # Uso de __name__ en lugar de _name_
+task_list = ['tasks 1', 'tasks 2', 'tasks 3']
 
+# Ruta para mostrar todas las tareas
 @taskRoute.route('/')
 def index():
-    return 'Index'
+    return render_template('dashboard/task/index.html', tasks=task_list)
 
+# Ruta para mostrar una tarea específica por ID
 @taskRoute.route('/<int:id>')
-def show(id:int):
-    return 'Show ' +str(id)
+def show(id: int):
+    return 'Show ' + str(id)
 
+# Ruta para eliminar una tarea específica
 @taskRoute.route('/delete/<int:id>')
-def delete(id:int):
-    return 'delete ' +str(id)
+def delete(id: int):
+    if id is not None and 0 <= id < len(task_list):  # Verificación de índice válido
+        del task_list[id]
+    return redirect(url_for('task.index'))
 
-@taskRoute.route('/create', methods=('GET','POST'))
+# Ruta para crear una nueva tarea
+@taskRoute.route('/create', methods=('GET', 'POST'))
 def create():
-    return 'Create '
+    task = request.form.get('task')
+    if task is not None and task != "":  # verifica que no venga vacia 
+        task_list.append(task)
+        return redirect(url_for('task.index'))
+    return render_template('dashboard/task/create.html')
 
-@taskRoute.route('/update/<int:id>', methods=['GET','POST'])
-def update(id:int):
-    return 'update '+str(id)
+# Ruta para actualizar una tarea específica
+@taskRoute.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id: int):
+    task = request.form.get('task')
+    if task is not None and task != "" and 0 <= id < len(task_list):  # Verificación de índice válido
+        task_list[id] = task
+        return redirect(url_for('task.index'))
+    return render_template('dashboard/task/update.html')
